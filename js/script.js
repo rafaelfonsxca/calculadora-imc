3ed77027f159b1336828ddfcb75b28a8b34810ef
Rafael Fonseca <rafael_010697@hotmail.com>
2020-06-30 10:32:40 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -5,27 +5,33 @@ function calcularImc(){
     var imc = peso / (altura * altura)
     imc = imc.toFixed(2)
 
-    var grau_imc = '';
+    var grau_imc = ''
+    var texto = ''
 
     if(imc < 18.5){
         grau_imc = "MAGREZA"
+        texto = "Você possui um baixo índice de massa corporal. Procure um nutricionista ou um especialista na áera da saúde para saber como melhorar seu IMC."
     }else if(imc >= 18.5 && imc <= 24.9){
         grau_imc = "NORMAL"
+        texto = "Muito bom, você está com um bom índice de massa corporal. Procure um nutriconista ou um especialista na área para saber como manter esse resultado."
     }else if(imc >= 25.0 && imc <= 29.9){
         grau_imc = "SOBREPESO"
+        texto = "Você está com o seu índice de massa corporal um pouco acima do que deveria. Procure um nutricionista ou um especialista na área para saber como melhorar seu IMC."
     }else if(imc >= 30.0 && imc <= 39.9){
         grau_imc = "OBESIDADE"
+        texto = "Você apresenta um índice de massa corporal bem acima do que deveria, mas não se preocupe que não é anda grave. Procure um nutricionista ou um especialista na área para saber como melhorar seu IMC."
     }else if(img > 40.0){
         grau_imc = "OBESIDADE GRAVE"
+        texto = "Você apresenta um índice de massa corporal bem acima do que deveria que já chega a ser grave. Procure com urgência um nutricionista ou um especialista na área para saber como melhorar seu IMC."
     }
 
     document.getElementById('imc').innerHTML = imc
     document.getElementById('grau_imc').innerHTML = grau_imc
+    document.getElementById('texto').innerHTML = texto
+
 }
 
 function limpar(){
     document.getElementById('peso').value = ''
     document.getElementById('altura').value = ''
-    document.getElementById('imc').value = ''
-    document.getElementById('grau_imc').value = ''
 }
